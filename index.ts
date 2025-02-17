2b978cb2d6287bbb471496d134a704e7518b6a02
RanMC-9918 <rohanjain0725@gmail.com>
2025-02-17 01:41:53 -0600

    first time ts setup

---
+++ index.ts
@@ -0,0 +1,33 @@
+import * as express from "express";
+const app = express();
+
+import path = require('path');
+
+import { REST, Routes } from "discord.js"
+
+import { Client, Events, GatewayIntentBits } from "discord.js"
+
+import ejs from "ejs";
+
+
+const port = 8080;
+
+
+app.set('view engine', 'ejs');
+
+app.use('/favicon.ico', express.static('views/images/favicon.ico'));
+
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/", (req, res) => {
+    res.render("home/index")   
+
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(`Server is running http://localhost:8080`);
+})
