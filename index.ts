6471ccf6963facd3438ae22acaa298c396e1a027
RanMC-9918 <rohanjain0725@gmail.com>
2025-02-28 13:15:57 -0600

    BIG UDATE (started bot input, finsished bracket rendering)

--- index.ts
+++ index.ts
@@ -3,11 +3,15 @@ const app = express();
 
 import path = require('path');
 
-import { REST, Routes } from "discord.js"
+import { REST, Routes, SlashCommandBuilder} from "discord.js"
 
 import { Client, Events, GatewayIntentBits } from "discord.js"
 
 import ejs from "ejs";
+import { register } from "module";
+
+import enviornment = require("dotenv");
+enviornment.config();
 
 
 const port = 8080;
@@ -18,6 +22,7 @@ interface bracket {
     players: string[];
     matchupsPlayed: number;
     matchups: boolean[];
+    ownerId: string;
 }
 
 let brackets: bracket[] = [];
@@ -35,8 +40,9 @@ brackets.push({
     "Player 7",
     "Player 8",
   ],
-  matchupsPlayed: 3,
+  matchupsPlayed: 6,
   matchups: [false, false, false, false, false, false, false],
+  ownerId: '723619381958934569',
 });
 
 
@@ -114,6 +120,134 @@ function mapPlayers(players: string[], matchups: boolean[], matchupsPlayed: numb
         );
         }
     }
+
+    console.log(mappedPlayers)
     return mappedPlayers;
 
 }
+
+//DISCORD
+
+const botID = "1345091131368673281";
+const guildID = "1245922280991883294";
+const token = process.env.TOKEN;
+
+const rest = new REST().setToken(token);
+
+const commands = [
+    {
+        name: "ping-dev",
+        description: "mentions the developer.",
+    },
+    new SlashCommandBuilder()
+        .setName("create-bracket")
+        .setDescription("Creates a bracket")
+        .addStringOption(
+            option => 
+            option.setName("title")
+            .setDescription("The title of the bracket")
+            .setRequired(true)
+        )
+        .addIntegerOption(
+            option =>
+            option.setName("players")
+            .setDescription("The number of players in the bracket")
+        )
+]
+
+const registerCommands = async () => {
+    try {
+        console.log("Started refreshing application (/) commands.");
+
+        await rest.put(
+            Routes.applicationGuildCommands(botID, guildID),
+            { body: commands },
+        );
+
+        console.log("Successfully reloaded application (/) commands.");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+registerCommands();
+
+const bot = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+
+bot.on("ready", () => {
+    console.log("Bot is ready.");
+});
+
+interface bracketThread {
+    bracketId: number;
+    messageId: string;
+}
+
+let bracketThreads: bracketThread[] = []
+
+bot.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    switch (commandName) {
+        case "ping-dev":
+            await interaction.reply("@RanMC9918 hello");
+            break;
+
+        case "create-bracket":
+            const id = Math.floor(Math.random() * 100000);
+
+            
+            await interaction.reply(
+              "Bracket created at " + "http://localhost:8080/brackets/" + id
+              + "\nThank you for using our service."
+            );
+
+            brackets.push({
+                id: id,
+                title: String(interaction.options.get("title").value),
+                players: [],
+                matchupsPlayed: 0,
+                matchups: [],
+                ownerId: interaction.user.id
+            });
+
+
+            const message = await interaction.followUp({content: "Join by reacting 🤚\nStart early by reacting ➡️ (only the owner)", fetchReply: true});
+            
+            await message.react("🤚");
+            await message.react("➡️")
+
+            bracketThreads.push({
+                bracketId: id,
+                messageId: message.id
+            });
+            
+            break; 
+    }
+});
+
+bot.on("messageReactionAdd", async (reaction, user) => {
+    if (user.bot) return;
+
+    console.log(user.username)
+
+    const bracketThread = bracketThreads.find(thread => thread.messageId === reaction.message.id);
+    const bracket = brackets.find(bracket => bracket.id == bracketThread.bracketId);
+    bracket.players.push(user.username);
+    bracket.matchups.push(false);
+
+    reaction.message.channel.isSendable() && reaction.message.channel.send(`<@${user.id}> has joined the bracket.`);
+
+})
+
+bot.login(token);
+
